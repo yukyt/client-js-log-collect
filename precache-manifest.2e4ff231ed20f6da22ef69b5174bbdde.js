92eef9ef585fed68ffd5c62d84630ce9ce015091
yukio.tani <yukio.tani@rakuten.com>
2019-05-28 13:45:49 +0900

    Updates

---
+++ precache-manifest.2e4ff231ed20f6da22ef69b5174bbdde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b139c1eebcbe556edb19d4009db08db2",
+    "url": "/client-js-log-collect/index.html"
+  },
+  {
+    "revision": "b3fd5b922330e213e61f",
+    "url": "/client-js-log-collect/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/client-js-log-collect/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "b3fd5b922330e213e61f",
+    "url": "/client-js-log-collect/static/js/main.becb2064.chunk.js"
+  },
+  {
+    "revision": "f2aacc79a4843a58d77d",
+    "url": "/client-js-log-collect/static/js/runtime~main.cb535e36.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/client-js-log-collect/static/media/logo.5d5d9eef.svg"
+  }
+]);
